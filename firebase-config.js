21663868cbc47d4e14c0e11f0ae3c0826650e4c9
rhyme0203 <rhyme0203@gmail.com>
2025-10-25 10:44:18 +0900

    feat: Firebase 실제 설정 정보 입력 - lucky-784fd 프로젝트 연결

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 // Firebase Realtime Database 설정 파일
-// Firebase 콘솔에서 받은 실제 설정으로 교체하세요
+// Firebase 콘솔에서 받은 실제 설정
 
 const firebaseConfig = {
-  apiKey: "여기에_실제_apiKey_입력",
-  authDomain: "여기에_실제_authDomain_입력",
-  databaseURL: "https://lucky-784fd-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "여기에_실제_projectId_입력",
-  storageBucket: "여기에_실제_storageBucket_입력",
-  messagingSenderId: "여기에_실제_messagingSenderId_입력",
-  appId: "여기에_실제_appId_입력"
+  apiKey: "AIzaSyBew3EgQ57RrxUjDjYDC6s_ecj49etpSVI",
+  authDomain: "lucky-784fd.firebaseapp.com",
+  databaseURL: "https://lucky-784fd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lucky-784fd",
+  storageBucket: "lucky-784fd.firebasestorage.app",
+  messagingSenderId: "836524677981",
+  appId: "1:836524677981:web:f2ffcc81e9d3619a5466b4"
 };
 
 // Firebase 설정 업데이트
